479313f64f00abccbd285769c24d9571ed3a5b07
Victor Sheff <v.sheff@dats.team>
2022-03-18 19:52:24 +0300

    Spring installed Bot initialized

---
+++ src/main/java/com/github/meistersky/telegram/TelegramGroupBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.meistersky.telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramGroupBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramGroupBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/meistersky/telegram/TelegramGroupBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.meistersky.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramGroupBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
