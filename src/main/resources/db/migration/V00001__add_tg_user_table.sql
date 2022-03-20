b7e1144b96d045a2417bcd202db8d7b1990d4023
Victor Sheff <v.sheff@dats.team>
2022-03-20 20:39:48 +0300

    Added SQL Migration

---
+++ src/main/resources/db/migration/V00001__add_tg_user_table.sql
@@ -0,0 +1,17 @@
+-- ensure that the table with this name is removed before creating a new one.
+DROP TABLE IF EXISTS user_group;
+DROP TABLE IF EXISTS tg_user;
+
+-- Create tg_user table
+CREATE TABLE tg_user (
+    chat_id INT PRIMARY KEY,
+    active BOOLEAN
+);
+
+-- Create tg_group table
+CREATE TABLE user_group (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    chat_id INT NOT NULL,
+    title VARCHAR(100),
+    users VARCHAR(100)
+);
